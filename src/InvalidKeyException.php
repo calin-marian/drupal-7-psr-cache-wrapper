d172fcb4f6f1ca01b8ddd5fbd483de51a0df1790
Calin Marian <calin.marian@dreamproduction.ro>
2016-12-02 12:12:11 +0100

    Fix comment in InvalidKeyException

--- src/InvalidKeyException.php
+++ src/InvalidKeyException.php
@@ -6,7 +6,7 @@ use Psr\Cache\InvalidArgumentException;
 
 /**
  * @file
- * Contains Drupal\places_api_autocomplete\Exception\InvalidKeyException.
+ * Contains DrupalPsr\Cache\InvalidKeyException.
  */
 
 /**
